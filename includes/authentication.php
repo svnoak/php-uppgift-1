ff8142df1eedad30c13ad93f4a94af4a4d7a7c12
Kim <kimpfimp@posteo.net>
2021-10-19 11:13:33 +0200

    removing dump

--- includes/authentication.php
+++ includes/authentication.php
@@ -2,7 +2,6 @@
 session_start();
 
 if( $_SERVER['REQUEST_URI'] == "/profile.php"  ){
-    var_dump($_SESSION['isLoggedIn']);
     if( !isset($_SESSION['isLoggedIn']) ){
         if( !$_SESSION['isLoggedIn'] ){
             $_SESSION['status'] = "Please log in to access this page";
