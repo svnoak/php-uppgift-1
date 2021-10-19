04468acf696d73ae33003ecc2f4c0d2f008ae8d7
Kim <kimpfimp@posteo.net>
2021-10-19 11:14:40 +0200

    adding a space

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once "includes/header.php";
 ?>
 <section>
     <h1>Welcome to IDDb!</h1>
-    <p>You can <a href="/sign-in.php">Sign in</a>or <a href="/list.php">List all of the dogs</a>.</p>
+    <p>You can <a href="/sign-in.php">Sign in</a> or <a href="/list.php">List all of the dogs</a>.</p>
 </section>
 <?php
 include_once "includes/footer.php";
